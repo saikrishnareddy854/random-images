7f7eec509185eca797d7c4f3ea5ef1427728cd56
saikrishnareddy854 <saikrishnareddy123.gurijala@gmail.com>
2023-03-08 23:14:10 +0530

    first commit

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const Quotes=[{
            quotes:`Take a much-needed break from your day to check out these 101 funny quotes we found in stand-up comedy, books, plays, celebrity Twitter and interviews as well as movies and TV shows, guaranteed to give you a quick chuckle.`,
             author:'sai'},
             
-                {quotes:`Take a much-needed t these 101 funny quotes we found in stand-up comedy, books, plays, celebrity Twitter and interviews as well as movies and TV shows, guaranteed to give you a quick chuckle.`,
-                 author:"saikrisnareddy"},
-                    
+                {quotes:`“Do one thing every day that scares you.`,
+                 author:"― Eleanor Roosevelt"},
+
+                 {quotes:`“We are what we pretend to be, so we must be careful about what we pretend to be.”`,
+                  author: " ― Kurt Vonnegut Mother Night"},
+
+                   {quotes:`“When we love, we always strive to become better than we are. When we strive to become better than we are, everything 
+                   around us becomes better too.”`,
+                author:"― Paulo Coelho, The Alchemist"}, 
                          ]
                  
 //access the elements
